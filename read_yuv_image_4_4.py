c1a783b2289d18ed8c7d2f32f4a7e057b0ee0185
José Frias <josefrias99@ua.pt>
2019-12-03 18:55:21 +0000

    yuv to rgb

--- read_yuv_image_4_4.py
+++ read_yuv_image_4_4.py
@@ -1,16 +1,15 @@
 import numpy as np
+from PIL import Image
+rgb=np.zeros((720,1280,3), dtype=np.uint8)
 def main():
     file='ducks_take_off_444_720p50.y4m'
     f=open(file, 'rb')
-    count_frame=0
 
     frame=f.readline().decode('utf-8')
     print (frame)
     header=f.readline()
     print (header.decode('utf-8'))
     while True:
-
-
 
         raw = f.read(int(1280*720*3))
         y = np.frombuffer(raw, dtype=np.uint8,count=1280*720)
@@ -20,14 +19,15 @@ def main():
         y = y.reshape(720, 1280)
         u=u.reshape(720,1280)
         v=v.reshape(720,1280)
-        print (len(y))
 
-        count_frame +=1
-        print (count_frame)
-        #nextline=f.readline()
-        #print (nextline)
-    print (count_frame)
 
+        for i in range(0, 720):
+            for j in range(0, 1280):
+                rgb[i][j]=[y[i][j]+1.14*v[i][j],y[i][j]-0.395*u[i][j]-0.581*v[i][j],y[i][j]+2.032*u[i][j]]
+
+        img = Image.fromarray(rgb, 'RGB')
+        img.save('my.png')
+        img.show()
 
 
 
